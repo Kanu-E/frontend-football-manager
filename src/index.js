40191ffa01b7084df5a290700bc46e48b0b01cb3
kanu-e <chukwuemekakanu@gmail.com>
2020-12-10 22:14:20 -0600

    display players

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("Liverpool FC")
+const players = document.getElementById("players")
+
+const played = ["Ronaldo", "Messi"]
+
+function showPlayers(player) {
+    for (let i = 0; i < player.length; i++){
+        displayPlayers(player[i])
+    }
+    
+}
+
+function displayPlayers(player){
+    let li = document.createElement('li'); 
+    li.innerHTML = player
+    players.appendChild(li)
+}
+
+
+
+showPlayers(played)
