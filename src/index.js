288faf4687cf1c1070bf8ee95c194b2cd6f233d8
kanu-e <chukwuemekakanu@gmail.com>
2020-12-10 23:52:29 -0600

    add ability to add new players

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const players = document.getElementById("players")
+const playersForm = document.getElementById("player-form")
 
-const played = ["Ronaldo", "Messi"]
+
+const played = [{name: "Ronaldo", number: 10}, {name:"Messi", number: 10}]
 
 function showPlayers(player) {
     for (let i = 0; i < player.length; i++){
@@ -11,10 +13,19 @@ function showPlayers(player) {
 
 function displayPlayers(player){
     let li = document.createElement('li'); 
-    li.innerHTML = player
+    li.innerHTML = `${player.name}, ${player.number}`
     players.appendChild(li)
 }
 
+playersForm.addEventListener("submit", addPlayer)
+
+function addPlayer(e){
+    e.preventDefault()
+    let player = {}
+    player.name = e.target[0].value
+    player.number = e.target[1].value
+    displayPlayers(player)
+}
 
 
 showPlayers(played)
