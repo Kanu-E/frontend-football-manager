62d71ca2d8a494a61cead69d2daf53f4319ee739
kanu-e <chukwuemekakanu@gmail.com>
2020-12-14 00:19:26 -0600

    add delete player function

--- src/index.js
+++ src/index.js
@@ -1,72 +1,78 @@
 const URL = "http://localhost:3000"
-const PLAYERURL = `${URL}/players`
+const playerURL = `${URL}/players`
 
 const players = document.getElementById("players")
 const playersForm = document.getElementById("player-form")
 
-// display players
-function showPlayers(player) {
-    for (let i = 0; i < player.length; i++){
-        displayPlayers(player[i])
-    }
-    
-}
-
-function displayPlayers(player){
-    // let li = document.createElement('li');
-    let p = document.createElement('p');
-    let card = document.createElement('div') 
-    let deleteButton = document.createElement('button')
-    deleteButton.innerHTML = 'delete'
-    // card.className = "card"
-    // li.innerHTML = `${player.name}, ${player.number}`
-    p.innerHTML = `${player.number}. ${player.name} `
-    p.className = "input-text"
-    // deleteButton.addEventListener('click')
-    card.append(p, deleteButton)
-    players.append(card)
-}
-
-// get players from database
-fetch(PLAYERURL)
-    .then(function (rsp) {
-        return rsp.json()
-    })
-    .then(function(obj){
-       showPlayers(obj)
-})
+// form to create new player
 
 playersForm.addEventListener("submit", addPlayer)
 
 function addPlayer(e){
     e.preventDefault()
-    let player = {}
-    player.name = e.target[0].value
-    player.number = e.target[1].value
+    let player = {
+        attributes: {
+            name: e.target[0].value,
+            number: e.target[1].value
+        }
+    }
     displayPlayers(player)
-    savePlayer(player)
+    savePlayer(player.attributes)
 }
 
+// display players
+function showPlayers(players) {
+    for (let i = 0; i < players.length; i++){
+        displayPlayers(players[i].data)
+    }    
+}
 
+function displayPlayers(player){
+    // console.log(player)
+    // let li = document.createElement('li');
+    let p = document.createElement('p');
+    let card = document.createElement('div');
+    let deleteButton = document.createElement('button');
+    deleteButton.innerHTML = 'delete'
+    p.dataset.id = player.id
+    // card.className = "card"
+    // li.innerHTML = `${player.name}, ${player.number}`
+    p.innerHTML = `${player.attributes.number}. ${player.attributes.name}`
+    p.className = "input-text"
+    deleteButton.addEventListener('click', deletePlayer)
+    card.append(p, deleteButton);
+    players.append(card);
+}
+
+// get players from database
+function fetchPlayers() {
+    fetch(playerURL)
+    .then(rsp => rsp.json())
+    .then(obj => showPlayers(obj));  
+}
 
 function savePlayer(player){
     let configObj = {
-    method: "POST",
-    headers: {
-        "content-type": "application/json",
-        "Accept": "application/json"
-    },
-    body: JSON.stringify({
-        name: player.name,
-        number: player.number
-    })
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: player.name,
+            number: player.number
+        })
     };
-    fetch(PLAYERURL, configObj)
+    fetch(playerURL, configObj)
 }
 
+function deletePlayer(e) {
+    const playerId = e.target.previousElementSibling.dataset.id
+        fetch(`${playerURL}/${playerId}`, 
+        {method: "DELETE"})
+    e.target.parentElement.remove()
+}
+    
 
- 
 
-// showPlayers(played)
-
-
+fetchPlayers()
