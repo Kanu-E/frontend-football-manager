230df0f41fa2e84f25e61c6d52d5cca81f999c1b
kanu-e <chukwuemekakanu@gmail.com>
2020-12-14 19:53:40 -0600

    create and read for game

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const URL = "http://localhost:3000"
 const playerURL = `${URL}/players`
-
+const gameURL = `${URL}/games`
 const players = document.getElementById("players")
 const playersForm = document.getElementById("player-form")
+const gamesForm = document.getElementById("game-form")
+const games = document.getElementById("games")
 
 // form to create new player
 
@@ -29,18 +31,20 @@ function showPlayers(players) {
 
 function displayPlayers(player){
     // console.log(player)
-    // let li = document.createElement('li');
     let p = document.createElement('p');
     let card = document.createElement('div');
     let deleteButton = document.createElement('button');
+    let selectButton = document.createElement('button');
     deleteButton.innerHTML = 'delete'
+    selectButton.innerHTML = 'select' 
     p.dataset.id = player.id
     // card.className = "card"
-    // li.innerHTML = `${player.name}, ${player.number}`
     p.innerHTML = `${player.attributes.number}. ${player.attributes.name}`
     p.className = "input-text"
     deleteButton.addEventListener('click', deletePlayer)
-    card.append(p, deleteButton);
+    selectButton.addEventListener('click', selectPlayer)
+
+    card.append(p, selectButton, deleteButton);
     players.append(card);
 }
 
@@ -72,7 +76,63 @@ function deletePlayer(e) {
         {method: "DELETE"})
     e.target.parentElement.remove()
 }
-    
 
 
+gamesForm.addEventListener("submit", addGame)
+
+function addGame(e) {
+     e.preventDefault()
+    let game = {
+        attributes: {
+            name: e.target[0].value,
+        }
+    }
+    displayGames(game)
+    saveGame(game.att)
+    // console.log(game)
+}
+
+function displayGames(game) {
+    let p = document.createElement('p')
+    p.innerHTML = game.attributes.name
+    let gameDiv = document.createElement('div')
+    gameDiv.append(p)
+    games.appendChild(gameDiv)
+
+}
+
+function fetchGames() {
+    fetch(gameURL)
+    .then(rsp => rsp.json())
+    .then(obj => showGames(obj));
+}
+
+function showGames(games) {
+    for (let i = games.length; i > 0; i--){
+        displayGames(games[i - 1].data)
+        // console.log(games)
+    }    
+}
+
+function saveGame(game){
+    // console.log(game)
+    let configObj = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: game.name
+        })
+    };
+    fetch(gameURL, configObj)
+}
+
+// function selectPlayer(e) {
+//     e.preventDefault()
+
+// }
+
+fetchGames()
 fetchPlayers()
